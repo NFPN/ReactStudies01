174ee84716b54730ba07517353daf68b855a12ab
Natanael Nunes <natanaelfpnunes@gmail.com>
2021-10-24 18:45:18 -0300

    add: card for children prop composition

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
+import Card from "./Card";
 import ExpanseDate from "./ExpanseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpanseDate date={props.date} />
       <div className="expense-item-description">
         <h2>{props.title}</h2>
         <div className="expense-item-price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
