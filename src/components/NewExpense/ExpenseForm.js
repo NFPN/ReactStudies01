daa2ab502fbfe5c02791868c3e8c0b5bbd0842fe
Natanael Nunes <natanaelfpnunes@gmail.com>
2021-10-24 22:00:55 -0300

    edit: states and two way binding to expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  //   const [userInput, setUserInput] = useState({
+  //     title: "",
+  //     amount: "",
+  //     date: "",
+  //   });
+
+  //   const titleChangeHandler = (event) => {
+  //     //this approach could get previous outdate states
+  //     // setUserInput({
+  //     //   ...userInput,
+  //     //   title: event.target.value,
+  //     // });
+
+  //     //this approach always get an updated state
+  //     setUserInput((prevState) => {
+  //       return { ...prevState, title: event.target.value };
+  //     });
+  //   };
+  //   const amountChangeHandler = (event) => {
+  //     setUserInput((prevState) => {
+  //       return { ...prevState, title: event.target.value };
+  //     });
+  //   };
+  //   const dateChangeHandler = (event) => {
+  //     setUserInput((prevState) => {
+  //       return { ...prevState, title: event.target.value };
+  //     });
+  //   };
+
+  const [title, setTitle] = useState();
+  const [amount, setAmount] = useState();
+  const [date, setDate] = useState();
+
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+      event.preventDefault();
+
+      const ExpenseData = {
+          title: title,
+          amount: amount,
+          date: new Date(date),
+      }
+      
+      setTitle('');
+      setAmount('');
+      setDate('');
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense-controls">
         <div className="new-expense-control">
           <label>Title</label>
-          <input type="text"></input>
+          <input type="text" value={title} onChange={titleChangeHandler}></input>
         </div>
         <div className="new-expense-control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={amount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense-control">
           <label>Date</label>
-          <input type="date" min="2021-11-01" max="2030-12-31"></input>
+          <input
+            type="date"
+            min="2021-11-01"
+            max="2030-12-31"
+            value={date}
+            onChange={dateChangeHandler}
+          ></input>
         </div>
       </div>
       <div className="new-expense-actions">
